57c41a5b9f3d1eab00f8f39c6cfee246bed54a3f
ramisettymahesh <ramisetty.venkatanagamaheshwar@cognizant.com>
2025-09-02 18:00:55 +0530

    use maven site command to create a project site

--- commands.sh
+++ commands.sh
@@ -1,4 +1,2 @@
-cd ..
-mvn archetype:generate -DgroupId=com.mahesh -DartifactId=my-maven-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-cd my-maven-app
-mvn package
+mvn site &&
+mvn site:run 
