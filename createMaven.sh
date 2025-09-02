c6561058ce0076a76a9d701ab35dc183bb8177b9
ramisettymahesh <ramisetty.venkatanagamaheshwar@cognizant.com>
2025-09-02 20:31:30 +0530

    Create a jar file of your project with maven

---
+++ createMaven.sh
@@ -0,0 +1,4 @@
+cd ..
+mvn archetype:generate -DgroupId=com.mahesh -DartifactId=my-maven-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false &&
+cd my-maven-app
+mvn package
